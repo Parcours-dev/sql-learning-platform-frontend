dfc2619942a2de5dd07811f59e2c850d89209594
Parcours <devaletpaul@gmail.com>
2024-03-19 19:15:41 +0100

    Suppression d'éléments visuels PRO + Modification des requêtes API pour match avec la nouvelle DB + Modification de la page Table pour afficher le contenu de la table Employe (test de concept)

--- src/pages/cards.vue
+++ src/pages/cards.vue
@@ -2,6 +2,20 @@
 import CardBasic from '@/views/pages/cards/card-basic/CardBasic.vue'
 import CardNavigation from '@/views/pages/cards/card-basic/CardNavigation.vue'
 import CardSolid from '@/views/pages/cards/card-basic/CardSolid.vue'
+
+
+
+import { ref } from 'vue'
+
+const cards = ref([
+  { title: 'Chapitre 1 ' },
+  { title: 'Chapitre 2' },
+  { title: 'Chapitre 3' },
+  { title: 'Chapitre 4' },
+])
+
+const hoveredCard = ref(null)
+
 </script>
 
 <template>
